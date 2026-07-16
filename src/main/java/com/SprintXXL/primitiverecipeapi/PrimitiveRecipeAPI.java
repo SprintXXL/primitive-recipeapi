e850e069391d8df20de853ee400a7ac819fd115b
SprintXXL <mspmain@pm.me>
2026-07-16 04:17:32 +0200

    .

--- src/main/java/com/SprintXXL/primitiverecipeapi/PrimitiveRecipeAPI.java
+++ src/main/java/com/SprintXXL/primitiverecipeapi/PrimitiveRecipeAPI.java
@@ -2,15 +2,11 @@ package com.SprintXXL.primitiverecipeapi;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import static com.SprintXXL.primitiverecipeapi.Reference.*;
 
 @Mod(modid = MODID, name = NAME, version = VERSION)
 public class PrimitiveRecipeAPI {
-
-    public static final Logger LOGGER = LogManager.getLogger(NAME);
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
